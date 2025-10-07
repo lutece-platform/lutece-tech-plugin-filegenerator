d65668f5abef1997385f3f16e8cf560702e1461e
Jeremy Deloche <jeremy.deloche@atos.com>
2025-10-07 15:36:50 +0200

    LUT-30866 : Liquibase tags correction

--- src/sql/plugins/filegenerator/core/init_core_filegenerator.sql
+++ src/sql/plugins/filegenerator/core/init_core_filegenerator.sql
@@ -1,4 +1,4 @@
---liquibase formatted sql
---changeset filegenerator:init_core_filegenerator.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset filegenerator:init_core_filegenerator.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO core_admin_right VALUES ('VIEW_TEMP_FILES','filegenerator.adminFeature.temporary_files.name',2,'jsp/admin/plugins/filegenerator/ManageMyFiles.jsp','filegenerator.adminFeature.temporary_files.description',0,NULL,'SYSTEM',NULL,NULL,3, 0);
--- src/sql/plugins/filegenerator/upgrade/update_db_filegenerator-1.0.1-1.0.2.sql
+++ src/sql/plugins/filegenerator/upgrade/update_db_filegenerator-1.0.1-1.0.2.sql
@@ -1,4 +1,4 @@
---liquibase formatted sql
---changeset filegenerator:update_db_filegenerator-1.0.1-1.0.2.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset filegenerator:update_db_filegenerator-1.0.1-1.0.2.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 UPDATE core_admin_right SET level_right=2 WHERE id_right='VIEW_TEMP_FILES';
